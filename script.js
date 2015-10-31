eca848acf76d3fe0913c0f7d63223845d2c95c9f
Scotland Weitzel <scotland.weitzel@west-mec.org>
2015-10-30 17:54:30 -0700

    Completed the entire project in one session of power coding. TAKE THAT ABSENCE!

---
+++ script.js
@@ -0,0 +1,26 @@
+val = 1;
+function formChanged(){
+    foodname = document.getElementsByName("food")[0].value;
+    description = document.getElementsByName("description")[0].value;
+    dateofpurchase = document.getElementsByName("purchase")[0].value;
+    expiration = document.getElementsByName("expiration")[0].value;
+}
+function addCells(){
+    var tbl = document.getElementById("table");
+    var row = tbl.insertRow(val);
+    var fn = row.insertCell(0);
+    var d = row.insertCell(1);
+    var dop = row.insertCell(2);
+    var ep = row.insertCell(3);
+    if (expiration <= 3){
+        row.id = "danger"
+    }
+    if (val%2 === 1){
+        row.id = "alternate"
+    }
+    fn.innerHTML = foodname;
+    d.innerHTML = description;
+    dop.innerHTML = dateofpurchase;
+    ep.innerHTML = expiration;
+    val = val + 1;
+}
